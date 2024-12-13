4075b215108f2103b81e9f9a8e7494fcd20ab759
Marlon Munoz <marlon@marlonshade.com>
2024-12-13 04:57:10 -0500

    README.md file moved

--- Client/src/components/Header.jsx
+++ Client/src/components/Header.jsx
@@ -3,7 +3,7 @@ import React from "react";
 function Header() {
     return (
         <div>
-            <h1>👶 Play-Time 🧸</h1>
+            <h1>👶 IRYNK-App  🧸</h1>
             <h4 className="sub-header">The Best Selection Of Toys For Child Development</h4>
         </div>
     )
