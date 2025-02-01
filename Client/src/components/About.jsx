75f88b7cf81d8a74f106bf183bb44724c6f46400
Marlon Munoz <marlon@marlonshade.com>
2025-01-31 20:41:38 -0500

    added more links in About.jsx

--- Client/src/components/About.jsx
+++ Client/src/components/About.jsx
@@ -5,7 +5,7 @@ function About () {
             <div className="card">
                 <div className="card-body text-dark">
                     <h5 className="card-title">Mission</h5>
-                    <p className="card-text">
+                    <p className="card-text border rounded">
                         {/* <em> */}
                         The mission, is to help new or first time parents choose a toy according to the baby's age
                         and development stage. I believe that toys are essential not only for their entertainment, but for 
@@ -14,13 +14,19 @@ function About () {
                         <span className="badge badge-success text-white">👶 IRYNK-App 🧸</span> was developed by a first-time parent for all parents.
                         {/* </em> */}
                     </p>
-                    <h5 className="card-title">Links</h5>
+                    <h5 className="card-title border rounded text-white bg-dark">Links</h5>
                     <div className="row">
                     <div className="col-md-6 mb-2">
                         <a href="https://lovevery.com/" className="btn btn-primary w-100" target="_blank" rel="noopener noreferrer">lovevery.com</a>
                     </div>
                     <div className="col-md-6 mb-2">
                         <a href="https://www.manhattantoy.com/" className="btn btn-primary w-100" target="_blank" rel="noopener noreferrer">manhattantoy.com</a>
+                    </div>
+                    <div className="col-md-6 mb-2">
+                        <a href="https://www.marlonshade.com/" className="btn btn-primary w-100" target="_blank" rel="noopener noreferrer">Marlon Shade</a>
+                    </div>
+                    <div className="col-md-6 mb-2">
+                        <a href="https://www.linkedin.com/in/marlonrmunoz/" className="btn btn-primary w-100" target="_blank" rel="noopener noreferrer">LinkeIn</a>
                     </div>
                     </div>
                 </div>
